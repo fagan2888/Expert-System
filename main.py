56e8cb0ea9ebce626cd17cf6132a420b8008dc02
gdannay <gregory.dannay@sciencespo.fr>
2018-03-29 23:16:46 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+class fact(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.conc = []
+        self.implies = []
+
+def start():
+    file_name = sys.argv[1]
+    with open(file_name, "r") as fd:
+        for line in fd:
+            print("Line = %s" % line)
+
+
+if __name__ == "__main__":
+    start()
